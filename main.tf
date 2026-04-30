e88725c2bf5f99eebfd7a1bb28cb3a86079c4cc8
Giganticida <j.orglinares2001@gmail.com>
2026-04-30 11:11:27 +0200

    hope

--- main.tf
+++ main.tf
@@ -32,14 +32,18 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "~> 8.0"
+  version = "~> 8.0" # Or "~> 9.0" for the absolute latest
 
   name = "blog"
 
   min_size            = 1
   max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  
+  # UPDATED REFERENCE:
+  # We access the 'blog_app' key from the target_groups output map
+  target_group_arns   = [module.blog_alb.target_groups["blog_app"].arn]
+  
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
@@ -58,7 +62,7 @@ module "blog_alb" {
   security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = {
-    {
+    blog_app = { # This 'blog_app' is your key name
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
@@ -66,11 +70,13 @@ module "blog_alb" {
     }
   }
 
-  http_tcp_listeners = {
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
+  listeners = {
+    http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "blog_app" # Matches the key in target_groups
+      }
     }
   }
 
