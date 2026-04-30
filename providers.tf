88af08153aa8087461d2887b43e6355780f8b705
Giganticida <j.orglinares2001@gmail.com>
2026-04-30 14:54:36 +0200

    macarrones

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
   region  = "us-west-2"
-  version = "~> 5.0"
 }
